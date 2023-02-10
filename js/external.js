b6892f564e7a271963030e1e20238b2f834962d1
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-02-10 14:02:19 -0600

    Added console logs to keep track of user's answers

--- js/external.js
+++ js/external.js
@@ -3,11 +3,12 @@
 console.log(`Hello from external JavaScript!`);
 
 alert(`Welcome to my website!`);
+let username = prompt(`What's your name?`);
 
 let favoriteColor = prompt(`I was thinking, what's your favorite color?`);
 console.log(`The user's favorite color is ${favoriteColor}`);
 
-alert(`You wouldn't believe it, but ${favoriteColor} is my favorite color!`);
+alert(`You wouldn't believe it, ${username}, but ${favoriteColor} is my favorite color!`);
 
 let littleMermaid = parseInt(prompt(`How many days do you want to rent The Little Mermaid?`)); // 3
 let brotherBear = parseInt(prompt(`How many days do you want to rent Brother Bear?`)); // 5
@@ -22,6 +23,7 @@ let totalCost = dailyCost * totalDaysRented;
 let formattedTotal = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"});
 
 alert(`Your total payment is ${formattedTotal}`);
+console.log(`${username} payed ${formattedTotal} to rent the movies.`)
 
 let googleRate = parseInt(prompt(`What is the hourly pay rate for Google?`)); // 400
 let amazonRate = parseInt(prompt(`What is the hourly pay rate for Amazon?`)); // 380
@@ -38,25 +40,26 @@ let totalPaycheck = googlePaycheck + amazonPaycheck + facebookPaycheck;
 let formattedPaycheck = totalPaycheck.toLocaleString("en-US", {style: "currency", currency: "USD"});
 
 alert(`Your paycheck this week will be ${formattedPaycheck}.`);
+console.log(`Your total paycheck is ${formattedPaycheck}.`)
 
 let student = prompt(`What is your name?`);
 let fullClass = confirm(`If there is an available seat in this class, press 'Ok'. If not, press 'Cancel'.`);
 let scheduleConflict = confirm(`If your schedule does not conflict with this class, press 'Ok'. If it does, press 'Cancel'`);
 
 if(!fullClass || !scheduleConflict) {
-    alert(`Sorry ${student}, that class is full, and you're schedule will conflict!`)
+    alert(`Sorry ${student}, that class is full, and you're schedule will conflict!`), console.log(`${student} could not be enrolled.`)
 } else {
-    alert(`Congratulations, ${student}, you're enrolled!`)
+    alert(`Congratulations, ${student}, you're enrolled!`), console.log(`${student} was successfully enrolled.`)
 };
 
 let offerExpired = confirm(`If this product is valid, press 'Ok. If it is expired, press 'Cancel' `)
 let productAmount = prompt(`How many products are you purchasing today?`)
-let memberStatus = confirm(`Are you a Gold Status Member?`)
+let memberStatus = confirm(`Are you a Gold Status Member? If so, press 'Ok'. If you are not, press 'Cancel'.`)
 
 if((productAmount >= 2 || memberStatus) && !offerExpired) {
-    alert(`Sorry Sir/Ma'am, this product offer cannot be redeemed today.`)
+    alert(`Sorry Sir/Ma'am, this product offer cannot be redeemed today.`), console.log(`Product offer could not be redeemed.`)
 } else {
-    alert(`Thank you. Your product offer has been redeemed.`)
+    alert(`Thank you. Your product offer has been redeemed.`), console.log(`Product offer redeemed.`)
 };
 
 
