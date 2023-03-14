68c74c0644528d7a26d1e338575c13a8da2b4a67
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-14 10:47:43 -0500

    DOM events lesson completed

--- js/dom-events-lesson.js
+++ js/dom-events-lesson.js
@@ -7,3 +7,11 @@ toggleMenu.addEventListener('click', function(){
 menuBg.addEventListener('click', function(){
     pageWrapper.classList.toggle('mobile-menu-open');
 });
+
+let dropdowns = document.querySelectorAll('[data-dropdown="parent"]') 
+    dropdowns.forEach(function (dropdown) {
+        let toggle = dropdown.querySelector('[data-dropdown="toggle"]')
+        toggle.addEventListener('click', function () {
+            dropdown.classList.toggle('open')
+        })
+    });
