d17d91fd0f314434e8c3ba0b062a6a334a343cff
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-14 11:07:34 -0500

    DOM events lesson completed

--- js/dom-events-lesson.js
+++ js/dom-events-lesson.js
@@ -9,9 +9,24 @@ menuBg.addEventListener('click', function(){
 });
 
 let dropdowns = document.querySelectorAll('[data-dropdown="parent"]') 
-    dropdowns.forEach(function (dropdown) {
-        let toggle = dropdown.querySelector('[data-dropdown="toggle"]')
-        toggle.addEventListener('click', function () {
-            dropdown.classList.toggle('open')
+dropdowns.forEach(function (dropdown) {
+    let toggle = dropdown.querySelector('[data-dropdown="toggle"]')
+    toggle.addEventListener('click', function () {
+        dropdowns.forEach(function (element) {
+            // if already has class of open, and is not in this element, remove
+            if (element.classList.contains('open') && element !== dropdown) {
+                element.classList.remove('open')
+            }
         })
+        dropdown.classList.toggle('open');
     });
+});
+
+pageWrapper.addEventListener('click', function (event) {
+    // if the event target is not a dropdown, close all dropdowns
+    if (!event.target.closest('[data-dropdown="parent"]')) {
+        dropdowns.forEach(function (dropdown) {
+            dropdown.classList.remove('open');
+        });
+    }
+});
--- js/utils.js
+++ js/utils.js
@@ -125,3 +125,28 @@ function abbrevName(name){
 const sortDrinkByPrice = drinks => {
     return drinks.sort((a, b) => a.price - b.price);
 }
+
+/** Hamburger Menu ---  */
+
+let dropdowns = document.querySelectorAll('[data-dropdown="parent"]')
+dropdowns.forEach(function (dropdown) {
+    let toggle = dropdown.querySelector('[data-dropdown="toggle"]')
+    toggle.addEventListener('click', function () {
+        dropdowns.forEach(function (element) {
+            // if already has class of open, and is not in this element, remove
+            if (element.classList.contains('open') && element !== dropdown) {
+                element.classList.remove('open')
+            }
+        })
+        dropdown.classList.toggle('open');
+    });
+});
+
+pageWrapper.addEventListener('click', function (event) {
+    // if the event target is not a dropdown, close all dropdowns
+    if (!event.target.closest('[data-dropdown="parent"]')) {
+        dropdowns.forEach(function (dropdown) {
+            dropdown.classList.remove('open');
+        });
+    }
+});
