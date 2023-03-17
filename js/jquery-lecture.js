ab8714bb2e953d32efac300a6b8d693c428ea507
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-17 10:39:04 -0500

    updating jquery lecture

--- js/jquery-lecture.js
+++ js/jquery-lecture.js
@@ -1,6 +1,6 @@
-jQuery("h3").click(function (){
-    $(this).next().slideToggle(500);
-});
+// jQuery("h3").click(function (){
+//     $(this).next().slideToggle(500);
+// });
 
 // if you want to use an arrow function, you cannot use
 // the 'this' keyword. In that case, you have to use
@@ -13,3 +13,29 @@ jQuery("h3").click(function (){
 // jQuery("h3").click((event)=>{
 //     $(event.target).next().slideToggle(500);
 // });
+
+// We can (and usually do) use '$' as shorthand for jQuery function
+// $("h3").click(function (){
+//     $(this).next().slideToggle(500);
+// });
+
+// If we ask our JS to access DOM elements but those elements have not loaded yet,
+// then our JS can't access them. We can get around this by asking our JS to
+// wait until the page has loaded.
+
+// We can do this with the vanilla JS window.onload event
+
+// function init() {
+//     $("h3").click(function () {
+//         $(this).next().slideToggle(500);
+//     });
+// }
+//
+// window.onload = init;
+
+// ORRRR we can use the $(document).ready
+$(document).ready(function (){
+    $("h3").click(function () {
+        $(this).next().slideToggle(500);
+    });
+});
