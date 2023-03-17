3f44541f1bf4ec18da81f6f2a74baea6ff604706
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-17 10:28:44 -0500

    downloaded jquery to local js files

--- js/jquery-lecture.js
+++ js/jquery-lecture.js
@@ -1,1 +1,15 @@
-"use strict"
+jQuery("h3").click(function (){
+    $(this).next().slideToggle(500);
+});
+
+// if you want to use an arrow function, you cannot use
+// the 'this' keyword. In that case, you have to use
+// event .currentTarget
+// arrow functions do NOT understand 'this'.
+// jQuery("h3").click((event)=>{
+//     $(event.currentTarget).next().slideToggle(500);
+// });
+// OR
+// jQuery("h3").click((event)=>{
+//     $(event.target).next().slideToggle(500);
+// });
