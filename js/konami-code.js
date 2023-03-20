9f25fbab39fda7db621b4c847d7e38fae94910f2
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-20 09:51:37 -0500

    completed konami code, moving onto making it 'fun'

--- js/konami-code.js
+++ js/konami-code.js
@@ -1,24 +1,47 @@
 "use strict";
 
-const allowedKeys = {
+/** Keymap **/
+let allowedKeys = {
     37: 'left',
     38: 'up',
     39: 'right',
     40: 'down',
     65: 'a',
-    66: 'b'
+    66: 'b',
+    13: 'enter'
 };
 
-const konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-const konamiCodePositioner = 0;
+let konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'enter'];
+let konamiCodePosition = 0;
 
-// keydown recognition
-$(document).keydown(function(e) {
+/** console logs keydowns **/
+$(document).keydown(function (e){
     console.log(e.keyCode);
-    if (e.keyCode == konamiCode) {
-        alert('99 LIVES ADDED!!!');
+});
+
+/** Event Listener **/
+document.addEventListener('keydown', function(e) {
+    // value of keycode for keymap
+    let key = allowedKeys[e.keyCode];
+    // value of required key for code
+    let requiredKey = konamiCode[konamiCodePosition];
+    // compare the key with required key
+    if (key == requiredKey) {
+        // move to next key in konami code
+        konamiCodePosition++;
+        // if the last key is reached, activate cheats
+        if (konamiCodePosition == konamiCode.length) {
+            activateCheats();
+            konamiCodePosition = 0;
+        }
+    } else {
+        konamiCodePosition = 0;
     }
 });
 
+function activateCheats() {
+    document.body.style.backgroundImage = "url('')";
 
+    alert("30 LIVES ADDED!!!");
+}
 
