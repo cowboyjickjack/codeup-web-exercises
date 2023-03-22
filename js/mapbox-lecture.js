de3b4ae744b16cd2a6e2aef5e3d53e424abd55f4
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-22 14:03:03 -0500

    completed map API lecture

--- js/mapbox-lecture.js
+++ js/mapbox-lecture.js
@@ -5,3 +5,18 @@ document.getElementById('zoomSubmit').addEventListener('click', event => {
     // this makes the button now respond to the input of the form
     map.setZoom(document.getElementById('zoom').value);
 });
+
+// enter an address button
+document.getElementById('setMarkerButton').addEventListener('click', event => {
+    // prevents form from submitting by clicking button, and resets it's function
+    event.preventDefault();
+    // this const stores the users input
+    const address = document.getElementById('setMarker').value;
+    console.log(address);
+    // geocode sets coordinates, .then stashes the coordinates as 'coords'
+    geocode(address, MAPBOX_API_TOKEN).then(coords=> {
+        const newMarker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+        // this centers the map on the input coords
+        map.setCenter(coords);
+    });
+})
