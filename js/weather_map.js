4727cc3790ae71421466efe2d12bb216050ff301
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-23 19:11:56 -0500

    added mapbox

--- js/weather_map.js
+++ js/weather_map.js
@@ -19,7 +19,7 @@
         return n;
     }
 
-    // forecasts weather
+    /** FORECASTS WEATHER **/
 
     $.get(`https://api.openweathermap.org/data/2.5/weather?lat=${SALat}&lon=${SALong}&appid=${WEATHERMAP_API_KEY}&units=imperial`).done(data=>{
         console.log(data);
@@ -31,5 +31,32 @@
     <div class="row no-gap">Max Temp: ${data.main.temp_min}</div>
     <div class="row no-gap">Min Temp: ${data.main.temp_max}</div>
     `);
+
+    /** ZOOM LEVELS **/
+    document.getElementById('5').addEventListener('click', event => {
+        map.flyTo({zoom:5});
+    });
+    document.getElementById('15').addEventListener('click', event => {
+        map.flyTo({zoom:15});
+    });
+    document.getElementById('20').addEventListener('click', event => {
+        map.flyTo({zoom:20});
+    });
+
+    /** ADDRESS INPUT/EVENT LISTENER **/
+    document.getElementById('setMarkerButton').addEventListener('click', event => {
+        // prevents form from submitting by clicking button, and resets its function
+        event.preventDefault();
+        // map.setZoom(document.getElementById('setMarker').value);
+
+        const address = document.getElementById('setMarker').value;
+        console.log(address);
+        // geocode sets coordinates, .then stashes the coordinates as 'coords'
+        geocode(address, MAPBOX_EXERCISE_TOKEN).then(coords=> {
+            const newMarker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+            // this centers the map on the input coords
+            map.setCenter(coords);
+        });
+    });
     });
 })();
