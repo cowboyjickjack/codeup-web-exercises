4987d590eca099c383b8db12c0ca444445b75590
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-27 16:16:37 -0500

    started ES6 lesson

---
+++ js/es6-lesson.js
@@ -0,0 +1,16 @@
+"use strict"
+// const HelloWorld = (name = 'World', ) => {
+//     if (!name) {
+//         name = 'World';
+//     }
+//     return `Hello, ${name}!`;
+// }
+// let response = HelloWorld();
+// console.log(response);
+//
+// const getPerson = async (id = 1) => {
+//     let response = await fetch(`https://swapi.dev/api/people/${id}`);
+//     let data = await response.json();
+//     console.log(data.name);
+// }
+
