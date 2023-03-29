ce3677b2e53e7b04d2d12d56ad0fcb7864d65e4b
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-29 14:09:35 -0500

    began promise lecture

--- js/github.js
+++ js/github.js
@@ -1,7 +1,12 @@
 export const getGitHubUsers = async () => { // export will allow this function to be exported // async will utilize await
-    // let response = await fetch('https://api.github.com/users'); // this exact url doesn't require a key
-    // console.log(response);
-    // return response;
-
-    return fetch('https://api.github.com/users').then(response => response.json()).then(data => data);
+    try { // try/catch is like if/else
+        let response = await fetch('https://api.github.com/users'); // this exact url doesn't require a key
+        let data = await response.json() // don't want to parse response variable(promise) until it's done resolving
+        // now you have the meat from the deli
+        return data;
+    } catch(error){
+        console.log(error);
+    }
+    // the .then syntax (older) compared to await
+    // return fetch('https://api.github.com/users').then(response => response.json()).then(data => data);
 }
--- js/promises-lecture.js
+++ js/promises-lecture.js
@@ -1,7 +1,9 @@
 import {getGitHubUsers} from "./github.js"; // can hold cmd, click, and takes you to the file
+// import {randomNumber} from "./utils-module"; // another example
 // {}} is specific for a function, not
 (async () =>{ // async will immediately run itself - needs no name, doesn't need to be called
-    let users = getGitHubUsers();
+    let users = await getGitHubUsers(); // this won't show unless you fire it off
+    // here is our meat :)
     console.log(users);
 
 
