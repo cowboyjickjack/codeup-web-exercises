05a7606530a000da94f79ca35c1e0635327e4b46
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-30 11:33:21 -0500

    practicing more API/promise work

--- js/promises2.js
+++ js/promises2.js
@@ -1,7 +1,7 @@
 "use strict"
 
 /*
- TODO: Using seperations of concerns principles, module syntax,
+ TODO: Using separations of concerns principles, module syntax,
      and async/await, create a series of code that:
       -   uses the StarWarsAPI: 'https://swapi.dev/'
       -   console logs a specific planet
@@ -10,5 +10,38 @@
       *BONUS* Add it into the DOM however you want
 */
 
-/** FETCHING **/
-const
+/** FETCHING PLANET INFO **/
+export const getStarWarsPlanets = async () => {
+    try {
+        let response = await fetch(`https://swapi.dev/api/planets/`);
+        let data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+export const specificPlanet = async (planetNumber) => {
+    try {
+        let response = await fetch(`https://swapi.dev/api/planets/${planetNumber}/`);
+        let data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+// console.log(specificPlanet(7));
+/** FETCHING EYE COLOR INFO **/
+export const getStarWarsCharacters = async () => {
+    try {
+        let response = await fetch(`https://swapi.dev/api/people/`);
+        let data = await response.json();
+        let people = data.results;
+        console.log(people);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+console.log(getStarWarsCharacters());
