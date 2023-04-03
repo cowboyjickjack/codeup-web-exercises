c3d24ce65d74c3aef446691667c19fc06099a42c
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-04-03 09:35:12 -0500

    continuing restful api lecture

--- js/movies.js
+++ js/movies.js
@@ -1,3 +1,60 @@
+export const getFavorites = async () => {
+    try {
+        let url = `http://localhost:3000/favorites`;
+        let options = {
+            headers: {
+                "Content-Type": `application/json`
+            },
+        }
+        let response = await fetch(url, options);
+        let data = response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getFavorite = async (id) => {
+    try {
+        let url = `http://localhost:3000/favorites/${id}`;
+        let options = {
+            headers: {
+                "Content-Type": `application/json`
+            },
+        }
+        let response = await fetch(url, options);
+        let data = response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const searchFavorite = async (movie) => {
+    let favorites = await getFavorites();
+
+    if (movie.title) {
+        let favorite = favorites.find((result) => { // condition we want to be matched
+            return movie.title === result.title;
+        });
+        if (favorite) {
+            return favorite;
+        } else {
+            return 'No movie was found with that title.';
+        }
+    }
+    else if (movie.genre) {
+        let favoritesFiltered = favorites.filter((result) => {
+           return movie.genre === result.genre;
+        });
+        if (favoritesFiltered.length > 0) {
+            return favoritesFiltered;
+        } else {
+            return 'No movies were found with that genre.';
+        }
+    }
+}
+
 export const setFavorite = async (postData) => {
     try {
         let url = `http://localhost:3000/favorites`;
@@ -6,7 +63,7 @@ export const setFavorite = async (postData) => {
             headers: {
                 "Content-Type": `application/json`
             },
-            body: JSON.stringify(movie) // zipping it all up, before it's sent
+            body: JSON.stringify(postData) // zipping it all up, before it's sent
         }
         let response = await fetch(url, options);
         let data = response.json();
@@ -28,6 +85,26 @@ export const patchFavorite = async (id, movie) => { // selecting id, replacing a
                 "Content-Type": `application/json/favorites/${id}`
             },
             body: JSON.stringify(movie) // zipping it all up, before it's sent
+        }
+        let response = await fetch(url, options);
+        let data = response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteFavorite = async (id) => {
+    try {
+        if (!id) { // since it's in try, it'll throw it into catch
+            throw new Error('You must provide an id')
+        }
+        let url = `http://localhost:3000/favorites`;
+        let options = {
+            method: "DELETE", // 'DELETE' since we're removing it
+            headers: {
+                "Content-Type": `application/json/favorites/${id}`
+            },
         }
         let response = await fetch(url, options);
         let data = response.json();
--- js/restful-api-lecture.js
+++ js/restful-api-lecture.js
@@ -1,4 +1,4 @@
-import {setFavorite, patchFavorite} from "./movies.js";
+import {setFavorite, patchFavorite, deleteFavorite, getFavorites, getFavorite} from "./movies.js";
 
 (async ()=> {
     document.querySelector('button').addEventListener('click', async function (){
@@ -13,14 +13,21 @@ import {setFavorite, patchFavorite} from "./movies.js";
             // DON'T SEND ID BECAUSE PPL DON'T KNOW HOW MANY MOVIES YOU HAVE
             // It'll be a guessing game on their end
         }
-        let result = await setFavorite(movieData);
-        console.log(result);
+        // let result = await setFavorite(movieData);
+        // console.log(result);
     });
 
-    let body = {
-        "rating": 2
-    }
-    let response = await patchFavorite(3, body);
-    let data = await response.json();
-    console.log(data);
+    // let body = {
+    //     "rating": 2
+    // }
+    // let response = await patchFavorite(3, body);
+    // let data = await response.json();
+    // console.log(data);
+
+    await deleteFavorite(4);
+    // let favorites = await getFavorites();
+    // console.log(favorites);
+    // let favorite = await getFavorite(2);
+    // console.log(favorite);
+
 })();
